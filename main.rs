fc7c646e858e9fd12f9383b19dd2c5e0ed870fb3
Noor Ul Ain Afaq <noorulain.afaqkhatri@gmail.com>
2024-08-29 18:31:38 +0500

    random secret number

--- main.rs
+++ main.rs
@@ -1,4 +1,15 @@
-fn main(){
- println!("Hello world!");
- println!("welcome to rust!!");
+use std::io;
+
+fn main() {
+    println!("Guess the secret number!");
+
+    println!("Please input your guess.");
+
+    let mut num = String::new();
+
+    io::stdin().read_line(&mut num).expect("Failed to take input");
+
+    let num:i32 = num.trim().parse().expect("enter a valid number");
+
+    println!("You guessed: {}", num);
 }
